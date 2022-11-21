486669e3a2d2b891dc72235ef8bbf8fb398f6eea
AllanEl <allanrnelias@gmail.com>
2022-11-20 20:52:39 -0500

    push to notes array finished

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const path = require('path');
 // creating the web server obj for organizing
-const app = express();
-// setting computer port number for service
 const jsonData = require('./Develop/db/db.json');
+const randomId = require('./helper/randomId.js');
 const PORT = 3001;
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// setting computer port number for service
+
 
 // installing middleware
 app.use(express.static('public'));
+
+notes = []
 
 app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
 
@@ -15,22 +24,53 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
+
+
 // app.get('/api/notes', (req, res) =>
 //   res.sendFile(path.join(__dirname, ''))
 // );
 
-app.get('/api/notes', (req, res) => res.json(jsonData));
+// app.get('/api/notes', (req, res) => res.json(jsonData));
+
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(jsonData);
+  });
+  
 
 app.post('/api/notes', (req, res) => {
-    // Let the client know that their POST request was received
-    res.json(`${req.method} request received`);
-  
-    // Show the user agent information in the terminal
-    console.info(req.rawHeaders);
-  
-    // Log our request to the terminal
-    console.info(`${req.method} request received`);
-  });
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+    console.log(req.body)
+   
+    // Destructuring assignment for the items in req.body
+    const {title, text} = req.body;
+    
+    
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        id: randomId(),
+        title,
+        text,
+      };
+      console.log(newNote)
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+      notes.push({newNote})
+      console.log(notes)
+      
+      
+    // JSON is a string, databases/file services/web servers work best with strings
+    
+    res.status(201).json(response);
+  } else {
+    // Catching the errors so we can send an error response
+    res.status(500).json('Error in posting Note');
+  }
+});
 
 // keep your web service awake and listening
 app.listen(PORT, () =>
